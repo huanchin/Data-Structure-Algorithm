1d53fbdf9f02ec68893dcac7da8fbe9a4840df2d
huan-chin chen <cherry032826@gmail.com>
2024-04-18 15:49:28 +0800

    add linked list

--- linkedList/app.js
+++ linkedList/app.js
@@ -25,6 +25,108 @@ class LinkedList {
     this.length++;
   }
 
+  pop() {
+    if (this.length === 0) {
+      return null;
+    } else if (this.length === 1) {
+      const temp = this.head;
+      this.head = null;
+      this.length--;
+      return temp;
+    } else {
+      let currentNode = this.head;
+      for (let i = 1; i < this.length - 1; i++) {
+        currentNode = currentNode.next;
+      }
+      const temp = currentNode.next;
+      currentNode.next = null;
+      this.length--;
+      return temp;
+    }
+  }
+
+  unshift(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      const temp = this.head;
+      this.head = newNode;
+      // newNode.next = temp;
+      this.head.next = temp;
+    }
+    this.length++;
+  }
+
+  shift() {
+    if (!this.head) return null;
+    else if (this.length === 1) {
+      const temp = this.head;
+      this.head = null;
+      this.length--;
+      return temp;
+    } else {
+      const temp = this.head;
+      this.head = this.head.next;
+      this.length--;
+      return temp;
+    }
+  }
+
+  insertAt(index, value) {
+    if (index > this.length - 1 || index < 0) {
+      return null;
+    } else if (index === 0) {
+      this.unshift(value);
+      return;
+    } else if (index === this.length - 1) {
+      this.push(value);
+      return;
+    }
+
+    const newNode = new Node(value);
+    let currentNode = this.head;
+    for (let i = 1; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
+    this.length++;
+    return;
+  }
+
+  removeAt(index) {
+    if (index > this.length - 1 || index < 0) {
+      return null;
+    } else if (index === 0) {
+      this.shift();
+      return;
+    } else if (index === this.length - 1) {
+      this.pop();
+      return;
+    }
+
+    let currentNode = this.head;
+    for (let i = 1; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+    const temp = currentNode.next;
+    currentNode.next = currentNode.next.next;
+    this.length--;
+    return temp.value;
+  }
+
+  get(index) {
+    if (index > this.length - 1 || index < 0) {
+      return null;
+    }
+    let currentNode = this.head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+    return currentNode.value;
+  }
+
   printAll() {
     if (this.length === 0) {
       console.log("Nothing in this linked list.");
@@ -42,5 +144,10 @@ let myLinkedList = new LinkedList();
 myLinkedList.push("Mike");
 myLinkedList.push("Harry");
 myLinkedList.push("James");
-myLinkedList.push("Kevin");
-myLinkedList.printAll();
+console.log(myLinkedList.get(2));
+// myLinkedList.push("Harry");
+// myLinkedList.push("James");
+// myLinkedList.push("Kevin");
+// myLinkedList.insertAt(2, "Ariel");
+// console.log(myLinkedList.removeAt(2));
+// myLinkedList.printAll();
